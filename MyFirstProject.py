7504d8589fd06ff562de0f485eb30aa4d693bbfa
Trond M. Pedersen <norwaytmp@gmail.com>
2014-01-14 02:32:03 +0100

    First outtake of my first own project.

---
+++ MyFirstProject.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+# This is my first own project. I have learned some basics python.
+# It`s time to put this to a test.
+# I know that this will most likely not be popular.
+# So I have no intensions of making this a public thing.
+# I will need to learn a few things on the way, but I know
+# most of the features I want to do before I begin.
+# Things will most likely come up during my prosess, but thats why I do this.
+# To learn.
+
+# Here we go.
+
+def menu():
+    # First menu, making a song name.
+    print ("Welcome to Guitar note maker.")
+    print (" ")
+    print ("First you need to make a name for your project.")
+    print ("Name your song: ")
+
+def menu2():
+    # Second menu, options avalible, apears after making a song name.
+    print ("A few more things is needed before you can start. ")
+    print (" ")
+    print ("Only downstrokes? (From up to down.)")
+# TODO: Less menues, try to manage with only 1 or 2. The long way for now.
+
+def menu3():
+    # Third menu, one more question.
+    print ("Only upstrokes? (From down to up.)")
+
+def menu4():
+    # Forth menu, one more question.
+    print ("Chose up or down after each note?")
+
+def menu5():
+    # Fifth menu, notes
+    print ("Chose the first note: ")
+    if choise = def choiseself
+    print ("(U)p or (D)own stroke? ")
+    elif = def choisenote
+    return input ("Chose next note: ")
+
+def choiceauto:
+    
+
+def choiseself:
+    
+
+def choiseway:
+    
+
+
+# The program it self.
+# Not sure how to define more menues yet, will do some research.
+loop = 1
+choise = 0
+while loop ==1:
+    choise = menu2()
+    if choice == 1:
+        choiceauto(input("You have chosen all notes")noteway=x)
+    elif choice == 2:
+        
+    elif choice == 3:
+        
+    elif choice == 4:
+        
+    elif choice == 5:
+
+        loop = 0
+
+print ("Thank you for using songmaker for guitar/base.")
+print ("This was made by Adversary  (norwaytmp@gmail.com)")
+print ("Copyrights to creator. All rights reserved.")
